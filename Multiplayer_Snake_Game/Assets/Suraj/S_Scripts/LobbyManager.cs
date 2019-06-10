125ef21343f41bd76a9cb503ebe3c6e616be98ea
Suraj Singh <surajss@uci.edu>
2019-06-09 22:39:05 -0700

    Networking Lobby Created
    The framework for the networking is in place.

---
+++ Multiplayer_Snake_Game/Assets/Suraj/S_Scripts/LobbyManager.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class LobbyManager : MonoBehaviourPunCallbacks
+{
+    public static LobbyManager lobby;
+    public GameObject randomRoomButton;
+    public GameObject cancelButton;
+
+
+    private void Awake()
+    {
+        // Creates a singleton
+        lobby = this;
+        // Don't allow join if not connected to server
+        randomRoomButton.SetActive(false);
+    }
+    
+    void Start()
+    {
+        // Connects to Master photon server
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Connected to Server!");
+        PhotonNetwork.AutomaticallySyncScene = true;
+        randomRoomButton.SetActive(true);
+    }
+
+    public void OnRandomRoomClicked ()
+    {
+        randomRoomButton.SetActive(false);
+        cancelButton.SetActive(true);
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        Debug.Log("Room not available or full");
+        CreateRoom();
+
+    }
+
+    void CreateRoom()
+    {
+        int randomRoom = Random.Range(0,100000);
+        RoomOptions roomOps = new RoomOptions(){IsVisible = true, IsOpen = true, MaxPlayers = (byte)MultiplayerSettings.multiplayerSetting.maxPlayers};
+        PhotonNetwork.CreateRoom("Room " + randomRoom, roomOps);
+    }
+
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        Debug.Log("Tried to create a room that already exist. Creating new room.");
+        CreateRoom();
+    }
+
+    public void OnCancelButtonClicked()
+    {
+        randomRoomButton.SetActive(true);
+        cancelButton.SetActive(false);
+        PhotonNetwork.LeaveRoom();
+    }
+}
---
+++ Multiplayer_Snake_Game/Assets/Suraj/S_Scripts/MultiplayerSettings.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MultiplayerSettings : MonoBehaviour
+{
+    public static MultiplayerSettings multiplayerSetting;
+    
+    public int maxPlayers;
+    public int menuScene;
+    public int multiplayerScene;
+
+    private void Awake() 
+    {
+        if (MultiplayerSettings.multiplayerSetting == null)
+        {
+            MultiplayerSettings.multiplayerSetting = this;
+        }
+        else
+        {
+            if (MultiplayerSettings.multiplayerSetting != this)
+            {
+                Destroy(this.gameObject);
+            }
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
